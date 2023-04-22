c269318a6e3d442728ff1874ebf90f463d2af23c
Lithanwa <j.santiago.523460@umindanao.edu.ph>
2023-04-22 12:45:45 +0800

    added db_connect.php and login.php

--- login.php
+++ login.php
@@ -1,2 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-include ('login.html');
+require ('LogIn.html');
+require ('db_connect.php');
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION['loggedin'] = true;
+        setcookie('userLogin', $username, time() + 3600);
+        header('Location: dashboard.php');
+        exit;
+    } else {
+        echo "<script>alert('Invalid login');</script>";
+    }
+}
+
+?>
