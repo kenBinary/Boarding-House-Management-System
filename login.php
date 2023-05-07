b6dd46d8fe5f5a3beff9245851a65464dfc44ce1
thennek0 <kennethbecaro0@gmail.com>
2023-05-07 17:36:41 +0800

    added case sensitive login

--- login.php
+++ login.php
@@ -1,17 +1,17 @@
 <?php
-require('login.html');
-require('db_connect.php');
+require ('login.html');
+require ('db_connect.php');
 session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+    $sql = "SELECT * FROM admin WHERE BINARY username like '$username' AND BINARY password like '$password'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) == 1) {
         $_SESSION['loggedin'] = true;
-        setcookie('userLogin', $username, time() + 3600);
+        setcookie('userLoginer', $username, time() + 5);
         header('Location: dashboard.php');
         exit;
     } else {
