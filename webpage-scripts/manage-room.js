5058364d5061ce07fcd91cd6883147608923650c
thennek0 <kennethbecaro0@gmail.com>
2023-05-10 21:44:41 +0800

    add function to create assign popUp

--- webpage-scripts/manage-room.js
+++ webpage-scripts/manage-room.js
@@ -2,6 +2,7 @@ let optionsArray = Array.from(document.querySelectorAll(".option"));
 let previewCardsArr = Array.from(document.querySelectorAll(".preview-card"));
 let currentOption = optionsArray[0];
 let previewSection = document.querySelector(".preview-section");
+let globalPopUp;
 optionsArray.forEach((option) => {
   option.addEventListener("click", (e) => {
     if (e.target != currentOption) {
@@ -116,29 +117,113 @@ function createPreviewDetails(parentElement, imageSource, textContent) {
     previewDetails.appendChild(detailImage);
     previewDetails.appendChild(detailText);
     if (previewDetails.textContent === "Assign") {
-        previewDetails.addEventListener('click',()=>{
-            console.log("test");
-        });
-    }
-    else if(previewDetails.textContent === "Remove"){
-        previewDetails.addEventListener('click',()=>{
-            console.log("bruh");
-        });
+      previewDetails.addEventListener("click", () => {
+        globalPopUp = createAssignPopUp();
+      });
+    } else if (previewDetails.textContent === "Remove") {
+      previewDetails.addEventListener("click", () => {
+        console.log("bruh");
+      });
     }
     parentElement.appendChild(previewDetails);
   }
 }
-function popUpAppear() {
+const popUpSection = () => {
   const parentElement = document.querySelector(".preview-section");
-  parentElement.classList.add("popup-appear");
-  return parentElement;
-}
-function createPopUpSection() {
-  const parentElement = popUpAppear();
   const popUpSection = document.createElement("div");
   popUpSection.classList.add("popup-modal-section");
-  const popUp = document.createElement("div");
+  const popUp = document.createElement("form");
   popUp.classList.add("pop-up");
   popUpSection.appendChild(popUp);
   parentElement.appendChild(popUpSection);
+  return { popUpSection, popUp };
+};
+function removePopUpSection(popUpSection) {
+  popUpSection.remove();
 }
+function createAssignPopUp() {
+  const sectionDiv = popUpSection();
+
+  let popUp = sectionDiv.popUp;
+  popUp.setAttribute("action", "test.php");
+  popUp.setAttribute("method", "post");
+
+  const title = document.createElement("div");
+  title.textContent = "Assign Room";
+
+  const divs = [];
+  for (let index = 0; index < 4; index++) {
+    const newDiv = document.createElement("div");
+    divs.push(newDiv);
+  }
+
+  const labels = [];
+  for (let index = 0; index < 3; index++) {
+    const label = document.createElement("label");
+    labels.push(label);
+  }
+
+  const inputs = [];
+  for (let index = 0; index < 5; index++) {
+    const input = document.createElement("input");
+    inputs.push(input);
+  }
+
+  //first div
+  let dataList = document.createElement("datalist");
+  dataList.setAttribute("id", "tenant-list");
+  const firstDivElements = [labels[0], inputs[0], dataList];
+  labels[0].setAttribute("for", "tenant");
+  labels[0].textContent = "Tenants";
+  inputs[0].setAttribute("list", "tenant-list");
+  inputs[0].setAttribute("name", "tenant");
+  inputs[0].setAttribute("id", "tenant");
+  firstDivElements.forEach((element) => {
+    divs[0].appendChild(element);
+  });
+  popUp.appendChild(divs[0]);
+
+  //second div
+  labels[1].setAttribute("for", "amenity");
+  labels[1].textContent = "Amenity";
+  inputs[1].setAttribute("type", "text");
+  inputs[1].setAttribute("name", "amenity");
+  inputs[1].setAttribute("id", "amenity-input");
+  inputs[1].setAttribute("value", "internet");
+  inputs[1].setAttribute("readonly", "");
+  divs[1].appendChild(labels[1]);
+  divs[1].appendChild(inputs[1]);
+  popUp.appendChild(divs[1]);
+
+  //third div
+  const thirdDivElements = [labels[2], inputs[2], inputs[3]];
+  labels[2].setAttribute("for", "utilities");
+  labels[2].textContent = "Utilities";
+  inputs[2].setAttribute("type", "text");
+  inputs[2].setAttribute("name", "utility-elec");
+  inputs[2].setAttribute("id", "electricity");
+  inputs[2].setAttribute("value", "Electricity");
+  inputs[2].setAttribute("readonly", "");
+  inputs[3].setAttribute("type", "text");
+  inputs[3].setAttribute("name", "utility-wat");
+  inputs[3].setAttribute("id", "water");
+  inputs[3].setAttribute("value", "Water");
+  inputs[3].setAttribute("readonly", "");
+  thirdDivElements.forEach((element) => {
+    divs[2].appendChild(element);
+  });
+  popUp.appendChild(divs[2]);
+
+  //fourth div
+  const closeDiv = document.createElement("div");
+  closeDiv.textContent = "Close";
+  inputs[4].setAttribute("type", "submit");
+  inputs[4].setAttribute("value", "submit");
+  inputs[4].setAttribute("name", "submit");
+  divs[3].appendChild(closeDiv);
+  divs[3].appendChild(inputs[4]);
+  popUp.appendChild(divs[3]);
+
+  return sectionDiv.popUpSection;
+}
+createAssignPopUp(removePopUpSection);
