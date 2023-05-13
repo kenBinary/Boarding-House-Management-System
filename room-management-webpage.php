2be529dc8b1213e099137489ed22d6eb73f4b68c
thennek0 <kennethbecaro0@gmail.com>
2023-05-13 20:35:33 +0800

    add php to assign tenant to room

--- room-management-webpage.php
+++ room-management-webpage.php
@@ -1,2 +1,10 @@
 <?php
 require('room-management.html');
+if (isset($_POST['assign-tenant'])) {
+    $tenant_ID = $_POST['tenant-list'];
+    $amenity = isset($_POST['amenity'])? $_POST['amenity']: "Null";
+    $room_number = $_POST['room_number'];
+    echo $tenant_ID;
+    echo $amenity;
+    echo $room_number;
+}
