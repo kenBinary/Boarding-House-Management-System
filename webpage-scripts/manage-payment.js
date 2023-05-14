fd500bc39c7024b56f7cef92967def90fe8e1e0a
thennek0 <kennethbecaro0@gmail.com>
2023-05-14 22:19:47 +0800

    add payment table style

--- webpage-scripts/manage-payment.js
+++ webpage-scripts/manage-payment.js
@@ -2,18 +2,19 @@ let optionsArray = Array.from(document.querySelectorAll('.option'));
 let previewCardsArr = Array.from(document.querySelectorAll('.preview-card'));
 let currentOption = optionsArray[0];
 let previewSection = document.querySelector('.preview-section');
-optionsArray.forEach(option => {
-    option.addEventListener('click', (e) => {
-        if (e.target != currentOption) {
-            currentOption = e.target.lastElementChild.textContent;
-            removeCards();
-        }
-    });
-});
-function removeCards() {
-    previewCardsArr.forEach((element) => {
-        let parentNode = element.parentElement;
-        parentNode.removeChild(element);
-    });
-    previewCardsArr.length = 0;
-}
+// optionsArray.forEach(option => {
+//     option.addEventListener('click', (e) => {
+//         if (e.target != currentOption) {
+//             currentOption = e.target.lastElementChild.textContent;
+//             removeCards();
+//         }
+//     });
+// // });
+// function removeCards() {
+//     previewCardsArr.forEach((element) => {
+//         let parentNode = element.parentElement;
+//         parentNode.removeChild(element);
+//     });
+//     previewCardsArr.length = 0;
+// }
+
